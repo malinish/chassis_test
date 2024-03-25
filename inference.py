cf2a1a3075165e2819a4d5b3ec7c274d41601e40
malinish <56129780+malinish@users.noreply.github.com>
2024-03-25 19:51:17 -0400

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,17 @@
+from chassis.client import OMIClient
+
+# Instantiate OMI Client connection to model running on localhost:45000
+client = OMIClient("localhost", 45000)
+
+# Call and view results of status RPC
+status = await client.status()
+print(f"Status: {status}")
+
+# Submit inference with quickstart sample data
+res = await client.run("22133")
+
+# Parse results from output item
+result = res.outputs[0].output["results.json"]
+
+# View results
+print(f"Result: {result}")
